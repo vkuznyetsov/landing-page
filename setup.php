b0651e0c86ed3df1f150ecfa8e9e418ad3b5f4f4
Viktor Kuznyetsov <vkuznyetsov@codenvy.com>
2015-06-16 16:20:00 +0300

    skip file operation if no data in post

--- setup.php
+++ setup.php
@@ -6,25 +6,27 @@
 	<script src="script/jquery.min.js"></script>
 </head>
 <body>
+<?php
+if (isset($_POST["dns"])) { ?>
 <p color='green'>Congratulations!!!</p>
 <img class="loading" src="images/wait.gif">
 <?php
-echo "<div style='color:blue;'>";
-echo '<p>email: ' . htmlspecialchars($_POST["email"])."</p>";
-echo '<p>dns: ' . htmlspecialchars($_POST["dns"])."</p>";
-echo '<p>password: ' . htmlspecialchars($_POST["password"])."</p>";
-echo "</div>";
-
-#exec ('/usr/local/landing/set-dns.sh ' . htmlspecialchars($_POST["dns"]) . ' ' . htmlspecialchars($_POST["email"]) . ' ' . htmlspecialchars($_POST["password"]));
-
-$fp = fopen('/usr/local/landing/results','w');
-fwrite($fp, htmlspecialchars($_POST["dns"]));
-fwrite($fp, PHP_EOL);
-fwrite($fp, htmlspecialchars($_POST["email"]));
-fwrite($fp, PHP_EOL);
-fwrite($fp, htmlspecialchars($_POST["password"]));
-fwrite($fp, PHP_EOL);
-fclose($fp);
+	echo "<div style='color:blue;'>";
+	echo '<p>email: ' . htmlspecialchars($_POST["email"])."</p>";
+	echo '<p>dns: ' . htmlspecialchars($_POST["dns"])."</p>";
+	echo '<p>password: ' . htmlspecialchars($_POST["password"])."</p>";
+	echo "</div>";
+	$fp = fopen('/usr/local/landing/results','w');
+	fwrite($fp, htmlspecialchars($_POST["dns"]));
+	fwrite($fp, PHP_EOL);
+	fwrite($fp, htmlspecialchars($_POST["email"]));
+	fwrite($fp, PHP_EOL);
+	fwrite($fp, htmlspecialchars($_POST["password"]));
+	fwrite($fp, PHP_EOL);
+	fclose($fp);
+} else {
+	echo "<p>Error. Can not found the DNS name. </p>";
+}
 
 #exec ('date 2>&1', $output);
 #print_r($output);  // to see the respond to your command
@@ -42,7 +44,7 @@ fclose($fp);
 		            window.location='http://<?php echo trim(htmlspecialchars($_POST["dns"]))?>';
 		        }else{
 		            window.console.error('API is not ready yet...');
-		            --maxAttempt;
+		            //--maxAttempt;
 		            if (!maxAttempt){
 			            $('.loading').addClass('hidden');
 			            clearInterval(intervalID);
